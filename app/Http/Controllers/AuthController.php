59571b37f2b61e70e42e3da420fd128e184e6337
Enrica Carpena <enricajulescarpena@gmail.com>
2022-10-07 10:40:17 +0800

    d

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -86,6 +86,7 @@ class AuthController extends Controller
             return response($response,201); // 201 Created
         }
     }
+}
 
          // Authenticate User
         public function login(Request $request){
